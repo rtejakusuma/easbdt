e8a58461cc91c54e5b4290c62f2bbb1bf5114ac3
root <rtejakusuma21@yahoo.com>
2019-12-05 22:59:09 +0700

    fixed

--- sql/elaporan.tipe_laporan-schema.sql
+++
@@ -1,7 +0,0 @@
-USE `elaporan`;
-CREATE TABLE `tipe_laporan` (
-  `id_tipe` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_laporan` varchar(64) DEFAULT NULL,
-  `kode_tipe` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`id_tipe`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
--- sql/elaporan.tipelaporan_per_opd-schema.sql
+++
@@ -1,9 +0,0 @@
-USE `elaporan`;
-CREATE TABLE `tipelaporan_per_opd` (
-  `id_opd` int(11) NOT NULL,
-  `id_tipe` int(11) NOT NULL,
-  PRIMARY KEY (`id_opd`,`id_tipe`),
-  KEY `fk_tipelaporan_per_opd2` (`id_tipe`),
-  CONSTRAINT `fk_tipelaporan_per_opd` FOREIGN KEY (`id_opd`) REFERENCES `opd` (`id_opd`),
-  CONSTRAINT `fk_tipelaporan_per_opd2` FOREIGN KEY (`id_tipe`) REFERENCES `tipe_laporan` (`id_tipe`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
